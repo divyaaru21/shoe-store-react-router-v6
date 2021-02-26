d44cf61366b683701ac079d6e75d36353c483bd1
SaadFarhanIdress <sunehrafarhan123@gmail.com>
2021-02-27 04:40:13 +0500

    Maintained credits and added css in it

--- src/components/home.js
+++ src/components/home.js
@@ -17,13 +17,31 @@ function Home() {
   const classes = useStyles();
   return (
     <>
-    <Typography className='heading'>
-      <br />
-      <br />
+      <Typography className='heading'>
+        <br />
+        <br />
       Shoe Store By Saad Farhan <br />
-      <Button color="inherit"><Link to='/product' className={classes.link}>Go to Products now!</Link></Button>
-    </Typography>
-  </>
+        <Button color="inherit"><Link to='/product' className={classes.link}>Go to Products now!</Link></Button>
+      </Typography>
+
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+
+      <p className='typo' style={{ textAlign: 'center' }}>
+        Made with
+      <img className="creditImage" width="20" height="20" alt="Heart" src="https://cdn.iconscout.com/icon/free/png-256/heart-1767836-1502416.png" />
+      by &nbsp;
+      <a href="http://www.github.com/SaadFarhanIdress">
+          Saad Farhan
+      </a>
+        <br />
+      Don't forget to star my repository :)
+      <br />
+      Link:
+      <a href="https://github.com/SaadFarhanIdress/project-3-shoe-store">
+          https://github.com/SaadFarhanIdress/project-3-shoe-store
+        </a>
+      </p>
+    </>
   );
 }
 
