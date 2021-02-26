b881947ae3336d731940e836ea4e07f16a79d9aa
SaadFarhanIdress <sunehrafarhan123@gmail.com>
2021-02-27 04:24:35 +0500

    Used useParams to fetch data of the product clicked by User and packed it in a const called shoe. Printed the name property, description property and img property of shoe.

--- src/components/productItems.js
+++ src/components/productItems.js
@@ -15,4 +15,29 @@ const useStyles = makeStyles((theme) => ({
     color: '#91612f',
   },
 }));
+
+function ProductItems() {
   const classes = useStyles();
+  const { id } = useParams();
+
+  const shoe = ShoesDetails[id]
+
+  if (!shoe) {
+    return <h2>Product not found....</h2>
+  }
+
+  return (
+    <div className='content'>
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+          <Paper className={cx(classes.paper, 'productBox')}><h2>{shoe.name}</h2><img src={shoe.img} height={500} alt={shoe.name} /></Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={cx(classes.paper, 'productBox')}><p>{shoe.description}</p></Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+export default ProductItems;
