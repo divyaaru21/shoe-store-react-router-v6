2b2f6ba1a5752076992c9e2bcb86c720380ee005
SaadFarhanIdress <saadfarhan347@gmail.com>
2022-09-10 03:56:34 +0500

    fix: move routes outside

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Home from '../components/Home';
-import About from '../components/About';
-import NavBar from '../components/Navbar';
-import ProductItems from '../components/Products';
-import Product from '../components/Product';
+import Home from './components/Home';
+import About from './components/About';
+import NavBar from './components/Navbar';
+import ProductItems from './components/Products';
+import Product from './components/Product';
 
 function RouteConfig() {
     return (
